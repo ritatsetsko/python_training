2fa158539fac3679b81fe424650543f552fbda13
ritatsetsko <eleevamv@yandex.ru>
2016-03-20 22:43:50 +0300

    Изменен с Python на PythonUnittest

--- test_add_group.py
+++ test_add_group.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
-import time
-
-success = True
-wd = WebDriver()
-wd.implicitly_wait(60)
+import time, unittest
 
 def is_alert_present(wd):
     try:
@@ -14,31 +10,40 @@ def is_alert_present(wd):
     except:
         return False
 
-try:
-    wd.get("http://localhost/addressbook/")
-    wd.find_element_by_name("user").click()
-    wd.find_element_by_name("user").clear()
-    wd.find_element_by_name("user").send_keys("admin")
-    wd.find_element_by_name("pass").click()
-    wd.find_element_by_name("pass").clear()
-    wd.find_element_by_name("pass").send_keys("secret")
-    wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-    wd.find_element_by_name("searchform").click()
-    wd.find_element_by_link_text("groups").click()
-    wd.find_element_by_name("new").click()
-    wd.find_element_by_name("group_name").click()
-    wd.find_element_by_name("group_name").clear()
-    wd.find_element_by_name("group_name").send_keys("groupname2")
-    wd.find_element_by_name("group_header").click()
-    wd.find_element_by_name("group_header").clear()
-    wd.find_element_by_name("group_header").send_keys("groupgeader")
-    wd.find_element_by_name("group_footer").click()
-    wd.find_element_by_name("group_footer").clear()
-    wd.find_element_by_name("group_footer").send_keys("groufooter")
-    wd.find_element_by_name("submit").click()
-    wd.find_element_by_link_text("group page").click()
-    wd.find_element_by_link_text("Logout").click()
-finally:
-    wd.quit()
-    if not success:
-        raise Exception("Test failed.")
+class test_add_group(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+    
+    def test_test_add_group(self):
+        success = True
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("groupname")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("groupheader")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("groupfooter")
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_link_text("Logout").click()
+        self.assertTrue(success)
+    
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
