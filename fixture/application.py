7a767ac6426ae365bcc5739aec74a9f95afd8847
ritatsetsko <eleevamv@yandex.ru>
2016-03-24 22:17:28 +0300

    Созданы пакеты для разных зон ответственности

--- fixture/application.py
+++ fixture/application.py
@@ -13,8 +13,7 @@ class Application:
 
     def create_group(self, group):
         wd = self.wd
-        # open groups name
-        wd.find_element_by_link_text("groups").click()
+        self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -31,6 +30,10 @@ class Application:
         wd.find_element_by_name("submit").click()
         # return to groups page
         wd.find_element_by_link_text("group page").click()
+
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
 
     def create_contact(self, contact):
         wd = self.wd
@@ -57,7 +60,7 @@ class Application:
 
     def login(self, username, password):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -66,6 +69,9 @@ class Application:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
 
     def destroy(self):
         self.wd.quit()
