f02dd8821b9847a33abb7a1a14a4aadbf9c65f9a
ritatsetsko <eleevamv@yandex.ru>
2016-04-03 12:47:51 +0300

    Оптимизация тестов путем изменения conftest.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,8 +3,7 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="Firstname", lastname="Lastname", mobile="89178765212", email="test@test.ru"))
-    app.session.logout()
+
 
 
