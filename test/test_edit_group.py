8c5ff3d31f280c3cbc9dd91eaffc0b6e80239b6b
ritatsetsko <eleevamv@yandex.ru>
2016-04-03 12:34:21 +0300

    Изменен тест по модификации группы

--- test/test_edit_group.py
+++
@@ -1,6 +0,0 @@
-from model.group import Group
-
-def test_edit_first_group(app):
-    app.session.login( username = "admin", password = "secret")
-    app.group.edit_first_group(Group(name ="groupname1", header ="groupheader2", footer ="groupfooter3"))
-    app.session.logout()
