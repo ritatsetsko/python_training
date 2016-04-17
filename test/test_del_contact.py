7b2bfde0691999d130b2920ba380f1e9a2590219
ritatsetsko <eleevamv@yandex.ru>
2016-04-17 18:03:00 +0300

    Проверка списков для удаления контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,4 +3,7 @@ from model.contact import Contact
 def test_delete_first_contact(app):
     if app.contact.count()==0:
         app.contact.create_contact(Contact(firstname="TestFirstname", lastname="TestLastname", mobile="89178765212", email="test@test.ru"))
+    old_contacts=app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contacts=app.contact.get_contact_list()
+    assert len(old_contacts)-1==len(new_contacts)
